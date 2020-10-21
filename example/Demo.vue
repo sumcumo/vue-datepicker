180145e047eb3ca54157bdb418458418e0fcfe9f
Sebastien <sebastienalbert977@hotmail.com>
2020-10-21 10:46:53 +0200

    docs(demo): Use number modifier to typecast as Number (#43)
    Co-authored-by: Sebastien Albert <sebastien.albert@inovim.eu>

--- example/Demo.vue
+++ example/Demo.vue
@@ -324,7 +324,7 @@
         <div class="form-group">
           <label>Year picker range:</label>
           <input
-            v-model="yearPickerRange"
+            v-model.number="yearPickerRange"
             type="number"
           >
         </div>
