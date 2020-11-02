6716a9e2aca26aac32e6b01f4b9f5d46e5a2e7e7
MrWook <mr.wook@web.de>
2020-11-02 11:02:39 +0100

    fix(scripts): get rid of sideeffects

--- src/locale/translations/ja.js
+++ src/locale/translations/ja.js
@@ -5,9 +5,9 @@ const language = new Language(
   ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
   ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
   ['日', '月', '火', '水', '木', '金', '土'],
+  false,
+  true,
+  '年',
 )
-
-language.yearSuffix = '年'
-language.ymd = true
 
 export default language
