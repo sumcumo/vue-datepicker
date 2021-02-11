35210ee3d8037ce33770401c80e7d09b15e668c7
mst101 <mst101@users.noreply.github.com>
2021-02-11 14:33:37 +0000

    test(pickerday): Get 100% coverage

--- test/unit/specs/PickerDay/pickerDay.spec.js
+++ test/unit/specs/PickerDay/pickerDay.spec.js
@@ -1,6 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import PickerDay from '~/components/PickerDay.vue'
-import { en } from '~/locale'
+import { en, mn } from '~/locale'
 
 describe('PickerDay: DOM', () => {
   let wrapper
@@ -32,9 +32,65 @@ describe('PickerDay: DOM', () => {
     expect(wrapper.emitted()['select-date']).toBeTruthy()
   })
 
+  it('knows the current page month', () => {
+    expect(wrapper.vm.pageMonth).toEqual(1)
+    expect(wrapper.vm.currMonthName).toEqual('Feb')
+
+    wrapper.setProps({
+      showFullMonthName: true,
+    })
+    expect(wrapper.vm.currMonthName).toEqual('February')
+  })
+
+  it('displays page title correctly', () => {
+    expect(wrapper.vm.pageTitleDay).toEqual('Feb 2018')
+
+    wrapper.setProps({
+      translation: mn, // Mongolian has dates in ymd format
+    })
+
+    expect(wrapper.vm.pageTitleDay).toEqual('2018 2-р сар')
+  })
+
   it('emits set-view event with `month` when the up button is clicked', () => {
     const upButton = wrapper.find('.day__month_btn')
     upButton.trigger('click')
     expect(wrapper.emitted()['set-view'][0][0]).toBe('month')
+  })
+
+  it('displays edge dates by default', () => {
+    const firstCell = wrapper.findAll('.day').at(0)
+    const lastCell = wrapper.findAll('.day').at(34)
+
+    expect(firstCell.text()).toBe('28')
+    expect(lastCell.text()).toBe('3')
+  })
+
+  it('does not display edge dates when show-edge-dates = false', async () => {
+    await wrapper.setProps({
+      showEdgeDates: false,
+    })
+
+    const firstCell = wrapper.findAll('.day').at(0)
+    const lastCell = wrapper.findAll('.day').at(34)
+
+    expect(firstCell.text()).toBe('')
+    expect(lastCell.text()).toBe('')
+  })
+
+  it('should select an edge date from the previous month', async () => {
+    const firstCell = wrapper.findAll('.day').at(0)
+
+    await firstCell.trigger('click')
+
+    expect(wrapper.emitted()['select-date'][0][0].date).toBe(28)
+  })
+
+  it('should select an edge date from the next month', async () => {
+    const lastCell = wrapper.findAll('.day').at(34)
+
+    await lastCell.trigger('click')
+
+    expect(wrapper.emitted()['select-date'][0][0].date).toBe(3)
   })
 })
