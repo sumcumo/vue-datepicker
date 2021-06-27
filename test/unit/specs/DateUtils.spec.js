2fc2be043f8d6f1b056364eefc75030227ed27e7
Mark Thompson <info@hiddengambia.com>
2021-06-27 17:54:28 +0200

    refactor(test): Other minor tweaks

--- test/unit/specs/DateUtils.spec.js
+++ test/unit/specs/DateUtils.spec.js
@@ -301,7 +301,7 @@ describe('UTC functions', () => {
   it('returns the correct day number from an abbreviated day name', () => {
     expect(dateUtils.getDayFromAbbr('sun')).toEqual(0)
     expect(dateUtils.getDayFromAbbr('sat')).toEqual(6)
-    expect(() => dateUtils.getDayFromAbbr('nonsense')).toThrow(
+    expect(() => dateUtils.getDayFromAbbr('invalid date')).toThrow(
       'Invalid week day',
     )
   })
