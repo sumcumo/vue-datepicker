106d75f7a6022b099b3997f1417f47fc3fd13de9
MrWook <mr.wook@web.de>
2021-08-16 15:15:44 +0200

    feat(typescript): add basic typescript setup

--- src/components/PickerCells.vue
+++ src/components/PickerCells.vue
@@ -11,52 +11,70 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'PickerCells',
-  props: {
-    cells: {
-      type: Array,
-      required: true,
-    },
-    showEdgeDates: {
-      type: Boolean,
-      default: true,
-    },
-    view: {
-      type: String,
-      validator: (val) => ['day', 'month', 'year'].includes(val),
-      required: true,
-    },
-  },
-  methods: {
-    /**
-     * Set the classes for a specific cell
-     * @return {Array}
-     */
-    // eslint-disable-next-line complexity
-    cellClasses(cell) {
-      return [
-        'cell',
-        this.view,
-        {
-          'disabled': cell.isDisabled,
-          'highlight-start': cell.isHighlightStart,
-          'highlight-end': cell.isHighlightEnd,
-          'highlighted': cell.isHighlighted,
-          'muted': cell.isPreviousMonth || cell.isNextMonth,
-          'sat': cell.isSaturday,
-          'sun': cell.isSunday,
-          'selected': this.showEdgeDates
-            ? cell.isSelected
-            : cell.isSelected && !cell.isPreviousMonth && !cell.isNextMonth,
-          'today': this.showEdgeDates
-            ? cell.isToday
-            : cell.isToday && !cell.isPreviousMonth && !cell.isNextMonth,
-          'weekend': cell.isWeekend,
-        },
-      ]
-    },
-  },
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+const showEdgeDatesValidator = (value: any): boolean => {
+  return ['day', 'month', 'year'].includes(value)
+}
+
+export interface Cell {
+  date: string
+  timestamp: number
+  isDisabled: boolean
+  isHighlightStart: boolean
+  isHighlightEnd: boolean
+  isHighlighted: boolean
+  isPreviousMonth: boolean
+  isNextMonth: boolean
+  isSaturday: boolean
+  isSunday: boolean
+  isSelected: boolean
+  isToday: boolean
+  isWeekend: boolean
+}
+
+@Component
+export default class PickerCells extends Vue {
+  @Prop() value: string | null
+
+  @Prop() cells: Cell[]
+
+  @Prop({
+    default: true,
+    validator: showEdgeDatesValidator,
+  })
+  showEdgeDates: boolean
+
+  @Prop() view: string
+
+  /**
+   * Set the classes for a specific cell
+   * @return {Array}
+   */
+  // eslint-disable-next-line complexity
+  cellClasses(cell: Cell) {
+    return [
+      'cell',
+      this.view,
+      {
+        'disabled': cell.isDisabled,
+        'highlight-start': cell.isHighlightStart,
+        'highlight-end': cell.isHighlightEnd,
+        'highlighted': cell.isHighlighted,
+        'muted': cell.isPreviousMonth || cell.isNextMonth,
+        'sat': cell.isSaturday,
+        'sun': cell.isSunday,
+        'selected': this.showEdgeDates
+          ? cell.isSelected
+          : cell.isSelected && !cell.isPreviousMonth && !cell.isNextMonth,
+        'today': this.showEdgeDates
+          ? cell.isToday
+          : cell.isToday && !cell.isPreviousMonth && !cell.isNextMonth,
+        'weekend': cell.isWeekend,
+      },
+    ]
+  }
 }
 </script>
