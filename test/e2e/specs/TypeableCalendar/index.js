4bc14e91aa27d165a92fdd5c25e28799da834af6
Mark Thompson <info@hiddengambia.com>
2022-06-29 11:15:05 +0200

    refactor(e2e): Wrap tests in outer describe block

--- test/e2e/specs/TypeableCalendar/index.js
+++ test/e2e/specs/TypeableCalendar/index.js
@@ -2,120 +2,122 @@ import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps'
 
 const { createCalendar, clickThe, focusThe, the } = cy
 
-describe('@id-1: Press the enter key', () => {
-  Given(
-    'the typeable calendar is {string} and a {string} date is typed',
-    (openOrClosed, validity) => {
-      const date = validity === 'valid' ? '1 March 2021' : 'invalid date'
-      const isNot = openOrClosed === 'closed' ? 'not.' : ''
+describe('Typeable calendar', () => {
+  describe('@id-1: Press the enter key', () => {
+    Given(
+      'the typeable calendar is {string} and a {string} date is typed',
+      (openOrClosed, validity) => {
+        const date = validity === 'valid' ? '1 March 2021' : 'invalid date'
+        const isNot = openOrClosed === 'closed' ? 'not.' : ''
 
-      createCalendar({
-        typeable: true,
-      })
+        createCalendar({
+          typeable: true,
+        })
 
-      if (openOrClosed === 'open') {
-        clickThe('input')
-      }
+        if (openOrClosed === 'open') {
+          clickThe('input')
+        }
 
-      focusThe('input').type(date)
+        focusThe('input').type(date)
 
-      the('calendar').should(`${isNot}be.visible`)
-    },
-  )
+        the('calendar').should(`${isNot}be.visible`)
+      },
+    )
 
-  When('the user presses the enter key', () => {
-    // Until Cypress supports native events, we can simulate this by blurring the input first
-    the('input').blur().type('{enter}')
-  })
+    When('the user presses the enter key', () => {
+      // Until Cypress supports native events, we can simulate this by blurring the input first
+      the('input').blur().type('{enter}')
+    })
 
-  Then('the calendar {string}', (opensOrCloses) => {
-    const isNot = opensOrCloses === 'closes' ? 'not.' : ''
-
-    the('calendar').should(`${isNot}be.visible`)
-  })
-
-  And('the date is {string}', (formattedOrCleared) => {
-    const value = formattedOrCleared === 'formatted' ? '01 Mar 2021' : ''
-
-    the('input').should('have.value', value)
-  })
-
-  And('the input has focus', () => {
-    the('input').should('have.focus')
-  })
-})
-
-describe('@id-2: Press the down arrow', () => {
-  Given(
-    'the typeable calendar is {string} and a {string} date is typed',
-    (openOrClosed, validity) => {
-      const date = validity === 'valid' ? '1 March 2021' : 'invalid date'
-      const isNot = openOrClosed === 'closed' ? 'not.' : ''
-
-      createCalendar({
-        typeable: true,
-      })
-
-      if (openOrClosed === 'open') {
-        clickThe('input')
-      }
-
-      focusThe('input').type(date)
+    Then('the calendar {string}', (opensOrCloses) => {
+      const isNot = opensOrCloses === 'closes' ? 'not.' : ''
 
       the('calendar').should(`${isNot}be.visible`)
-    },
-  )
+    })
 
-  When('the user presses the `down` arrow', () => {
-    focusThe('input').type(`{downArrow}`)
+    And('the date is {string}', (formattedOrCleared) => {
+      const value = formattedOrCleared === 'formatted' ? '01 Mar 2021' : ''
+
+      the('input').should('have.value', value)
+    })
+
+    And('the input has focus', () => {
+      the('input').should('have.focus')
+    })
   })
 
-  Then('the calendar {string}', (opensOrCloses) => {
-    const isNot = opensOrCloses === 'closes' ? 'not.' : ''
+  describe('@id-2: Press the down arrow', () => {
+    Given(
+      'the typeable calendar is {string} and a {string} date is typed',
+      (openOrClosed, validity) => {
+        const date = validity === 'valid' ? '1 March 2021' : 'invalid date'
+        const isNot = openOrClosed === 'closed' ? 'not.' : ''
 
-    the('calendar').should(`${isNot}be.visible`)
-  })
+        createCalendar({
+          typeable: true,
+        })
 
-  And('the date is {string}', (formattedOrCleared) => {
-    const value = formattedOrCleared === 'formatted' ? '01 Mar 2021' : ''
+        if (openOrClosed === 'open') {
+          clickThe('input')
+        }
 
-    the('input').should('have.value', value)
-  })
+        focusThe('input').type(date)
 
-  And('the previous button has focus', () => {
-    the('previous-button').should('have.focus')
-  })
-})
+        the('calendar').should(`${isNot}be.visible`)
+      },
+    )
 
-describe('@id-3: Press the down arrow (no header)', () => {
-  Given(
-    'the typeable calendar with no header is {string} and a {string} date is typed',
-    (openOrClosed, validity) => {
-      const date = validity === 'valid' ? '1 March 2021' : 'invalid date'
-      const isNot = openOrClosed === 'closed' ? 'not.' : ''
+    When('the user presses the `down` arrow', () => {
+      focusThe('input').type(`{downArrow}`)
+    })
 
-      createCalendar({
-        showHeader: false,
-        typeable: true,
-      })
-
-      if (openOrClosed === 'open') {
-        clickThe('input')
-      }
-
-      focusThe('input').type(date)
+    Then('the calendar {string}', (opensOrCloses) => {
+      const isNot = opensOrCloses === 'closes' ? 'not.' : ''
 
       the('calendar').should(`${isNot}be.visible`)
-    },
-  )
+    })
 
-  When('the user presses the `down` arrow')
+    And('the date is {string}', (formattedOrCleared) => {
+      const value = formattedOrCleared === 'formatted' ? '01 Mar 2021' : ''
 
-  Then('the calendar {string}')
+      the('input').should('have.value', value)
+    })
 
-  And('the date is {string}')
+    And('the previous button has focus', () => {
+      the('previous-button').should('have.focus')
+    })
+  })
 
-  And('the tabbable cell has focus', () => {
-    the('tabbable-cell').should('have.focus')
+  describe('@id-3: Press the down arrow (no header)', () => {
+    Given(
+      'the typeable calendar with no header is {string} and a {string} date is typed',
+      (openOrClosed, validity) => {
+        const date = validity === 'valid' ? '1 March 2021' : 'invalid date'
+        const isNot = openOrClosed === 'closed' ? 'not.' : ''
+
+        createCalendar({
+          showHeader: false,
+          typeable: true,
+        })
+
+        if (openOrClosed === 'open') {
+          clickThe('input')
+        }
+
+        focusThe('input').type(date)
+
+        the('calendar').should(`${isNot}be.visible`)
+      },
+    )
+
+    When('the user presses the `down` arrow')
+
+    Then('the calendar {string}')
+
+    And('the date is {string}')
+
+    And('the tabbable cell has focus', () => {
+      the('tabbable-cell').should('have.focus')
+    })
   })
 })
