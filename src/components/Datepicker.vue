054b0fedb05ec279fb3b1a5ee2e397aceb4f5544
Mark Thompson <info@hiddengambia.com>
2022-07-20 10:47:29 +0200

    fix(typeable): Set correct initial value of `latestValidTypedDate`

--- src/components/Datepicker.vue
+++ src/components/Datepicker.vue
@@ -609,8 +609,10 @@ export default {
           this.$emit('input', parsedValue)
         }
         this.setValue(parsedValue)
-      } else if (this.typeable) {
-        this.latestValidTypedDate = this.computedOpenDate
+      }
+
+      if (this.typeable) {
+        this.latestValidTypedDate = this.selectedDate || this.computedOpenDate
       }
 
       if (this.isInline) {
