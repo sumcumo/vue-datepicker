a0ea819483a5145f6d3a47e754ad8792857dfb3f
Mark Thompson <info@hiddengambia.com>
2022-08-13 16:07:41 +0200

    refactor(upbutton): Tidy up `UpButton`

--- src/components/UpButton.vue
+++ src/components/UpButton.vue
@@ -5,10 +5,10 @@
     :disabled="isDisabled"
     type="button"
     @click="$emit('select')"
-    @keydown.down.prevent="$emit('set-focus', ['tabbableCell'])"
-    @keydown.up.prevent="$emit('focus-input')"
-    @keydown.left.prevent="$emit('set-focus', [isRtl ? 'next' : 'prev'])"
-    @keydown.right.prevent="$emit('set-focus', [isRtl ? 'prev' : 'next'])"
+    @keydown.down.prevent="focusTabbableCell"
+    @keydown.up.prevent="focusInput"
+    @keydown.left.prevent="focusLeftButton"
+    @keydown.right.prevent="focusRightButton"
   >
     <slot />
   </button>
@@ -25,6 +25,28 @@ export default {
     isRtl: {
       type: Boolean,
       required: true,
+    },
+  },
+  computed: {
+    leftButton() {
+      return [this.isRtl ? 'next' : 'prev']
+    },
+    rightButton() {
+      return [this.isRtl ? 'prev' : 'next']
+    },
+  },
+  methods: {
+    focusInput() {
+      this.$emit('focus-input')
+    },
+    focusTabbableCell() {
+      this.$emit('set-focus', ['tabbableCell'])
+    },
+    focusLeftButton() {
+      this.$emit('set-focus', this.leftButton)
+    },
+    focusRightButton() {
+      this.$emit('set-focus', this.rightButton)
     },
   },
 }
